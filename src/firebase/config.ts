5e645f6198103516e0be32d26031814a0f097ea4
Matthew Baumann <matthewcb4@gmail.com>
2025-11-09 15:58:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8653444803-84660",
+  "appId": "1:196694469805:web:151c73d4b909b5869caa24",
+  "apiKey": "AIzaSyC-NGAxDVZb9e-u6JqfobprJaOzwH_2on8",
+  "authDomain": "studio-8653444803-84660.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "196694469805"
+};
