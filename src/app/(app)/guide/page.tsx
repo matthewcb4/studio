84bc8e8bf118f558121d72b141880c3ddef41122
Matthew Baumann <matthewcb4@gmail.com>
2025-11-14 20:35:08 +0000

    Okay, did you happen to identify what is causing the AI guide page to no

--- src/app/(app)/guide/page.tsx
+++ src/app/(app)/guide/page.tsx
@@ -82,7 +82,7 @@ export default function GuidePage() {
       availableEquipment: [],
       fitnessGoals: 'Build Muscle',
       fitnessLevel: 'intermediate',
-      workoutDuration: 45,
+      workoutDuration: 40,
       focusArea: ["Full Body"],
       focusOnSupersets: false,
     },
@@ -390,11 +390,14 @@ export default function GuidePage() {
                                 </SelectTrigger>
                                 </FormControl>
                                 <SelectContent>
-                                    <SelectItem value="15">15</SelectItem>
+                                    <SelectItem value="10">10</SelectItem>
+                                    <SelectItem value="20">20</SelectItem>
                                     <SelectItem value="30">30</SelectItem>
-                                    <SelectItem value="45">45</SelectItem>
+                                    <SelectItem value="40">40</SelectItem>
+                                    <SelectItem value="50">50</SelectItem>
                                     <SelectItem value="60">60</SelectItem>
-                                    <SelectItem value="75">75</SelectItem>
+                                    <SelectItem value="70">70</SelectItem>
+                                    <SelectItem value="80">80</SelectItem>
                                     <SelectItem value="90">90</SelectItem>
                                 </SelectContent>
                             </Select>
