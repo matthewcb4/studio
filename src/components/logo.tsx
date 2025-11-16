a4dad99b05fa02f91134af8b8623b4f5d5759f0a
Matthew Baumann <matthewcb4@gmail.com>
2025-11-16 15:34:39 +0000

    Can we use the icon for the main page and application icon?

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 const Logo = (props: ComponentProps<'div'>) => (
   <div {...props}>
     <Image
-      src="https://raw.githubusercontent.com/matthewcb4/public_resources/main/fOrganized_L.png"
+      src="https://raw.githubusercontent.com/matthewcb4/public_resources/481c0c57077c43a2a965c8531eb3fc1d60863e07/icon.png"
       alt="fOrganized Logo"
       width={400}
       height={400}
