f5e3511cd7750843efd671f577ebaf0651533402
Matthew Baumann <matthewcb4@gmail.com>
2025-11-22 23:23:02 +0000

    I got an error when trying to publish?
    Expected the first argument to b

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React, { DependencyList, createContext, useContext, ReactNode, useMemo, useState, useEffect } from 'react';
@@ -157,6 +158,7 @@ export const useFirebaseApp = (): FirebaseApp => {
 type MemoFirebase <T> = T & {__memo?: boolean};
 
 export function useMemoFirebase<T>(factory: () => T, deps: DependencyList): T | (MemoFirebase<T>) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoized = useMemo(factory, deps);
   
   if(typeof memoized !== 'object' || memoized === null) return memoized;
