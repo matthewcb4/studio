7c0958e0f67f986a49a5575c0fc617a557fffd24
Matthew Baumann <matthewcb4@gmail.com>
2025-11-24 14:59:00 +0000

    Try fixing this error: `Console Error: An empty string ("") was passed t

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -80,7 +80,7 @@ function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src={user?.photoURL || ''} alt={user?.displayName || 'User'} />
+            <AvatarImage src={user?.photoURL || undefined} alt={user?.displayName || 'User'} />
             <AvatarFallback>{!isUserLoading && getInitials()}</AvatarFallback>
           </Avatar>
         </Button>
