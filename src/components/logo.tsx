ed35ab5f35f909025d2dc6f0c395d130e928b2ec
Matthew Baumann <matthewcb4@gmail.com>
2025-11-24 16:47:29 +0000

    what needs to be done to fix this?

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 const Logo = (props: ComponentProps<'div'>) => (
   <div {...props}>
     <Image
-      src="/icons/icon-512x512.png"
+      src="/icons/icon.png"
       alt="fRepo Logo"
       width={40}
       height={40}
