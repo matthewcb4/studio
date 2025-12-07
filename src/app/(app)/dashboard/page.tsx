e65679dde8b30b71c8f2562fbd4f082dbe424b40
matthewcb4 <matthewcb4@gmail.com>
2025-12-07 17:26:24 -0600

    Fix dashboard mobile layout: add grid-cols-1

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -390,7 +390,7 @@ export default function DashboardPage() {
                     </div>
                 </div>
 
-                <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                     <UserStatsCard userProfile={userProfile} />
 
                     <Card className="lg:col-span-1 flex flex-col">
