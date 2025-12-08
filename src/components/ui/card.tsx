5f2b561abb77e8504f2aaecbc13a57228624f0a6
matthewcb4 <matthewcb4@gmail.com>
2025-12-08 15:49:57 -0600

    style: add drop shadow to widgets for better visual separation

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm shadow",
       className
     )}
     {...props}
@@ -79,4 +79,3 @@ CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
 
-    
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const prodConfig = {
   "projectId": "studio-8653444803-84660",
   "appId": "1:196694469805:web:151c73d4b909b5869caa24",
   "apiKey": "AIzaSyC-NGAxDVZb9e-u6JqfobprJaOzwH_2on8",
@@ -6,3 +6,15 @@ export const firebaseConfig = {
   "measurementId": "",
   "messagingSenderId": "196694469805"
 };
+
+const devConfig = {
+  "projectId": "tonal-studio-dev",
+  "appId": "1:830684740104:web:4a20ddbc78b50aa4edafbd",
+  "storageBucket": "tonal-studio-dev.firebasestorage.app",
+  "apiKey": "AIzaSyBOlEL0h4Ywo68Lbv7Q6VGQbyqkUQRXVo0",
+  "authDomain": "tonal-studio-dev.firebaseapp.com",
+  "messagingSenderId": "830684740104",
+  "measurementId": ""
+};
+
+export const firebaseConfig = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
