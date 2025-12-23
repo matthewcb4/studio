e29cf5267cd87494493d32b8560ad198306d7685
matthewcb4 <matthewcb4@gmail.com>
2025-12-23 13:19:02 -0600

    Register FirebaseAuthenticationPlugin in MainActivity

--- android/app/src/main/java/app/frepo/fitness/MainActivity.java
+++ android/app/src/main/java/app/frepo/fitness/MainActivity.java
@@ -2,12 +2,14 @@ package app.frepo.fitness;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Register the Health Connect plugin
+        // Register plugins
         registerPlugin(HealthConnectPlugin.class);
+        registerPlugin(FirebaseAuthenticationPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
