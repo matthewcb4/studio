992702a3b99d5356fce406fff35ddd85bc744b49
matthewcb4 <matthewcb4@gmail.com>
2025-12-28 18:32:19 -0600

    fix: add WindowInsets status bar padding to MainActivity, bump to 2.0.9 (19)

--- android/app/src/main/java/app/frepo/twa/MainActivity.java
+++ android/app/src/main/java/app/frepo/twa/MainActivity.java
@@ -1,6 +1,11 @@
 package app.frepo.twa;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
 import com.getcapacitor.BridgeActivity;
 import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
 
@@ -11,5 +16,13 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(HealthConnectPlugin.class);
         registerPlugin(FirebaseAuthenticationPlugin.class);
         super.onCreate(savedInstanceState);
+
+        // Ensure content doesn't overlap with system bars
+        View decorView = getWindow().getDecorView();
+        ViewCompat.setOnApplyWindowInsetsListener(decorView, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
     }
 }
